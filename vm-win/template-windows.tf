45853c84daac8a3b779de6dd9f3c7ebdde75372a
JS Park <jsp@hashicorp.com>
2020-07-16 14:40:23 +0900

    Update template-windows.tf

--- vm-win/template-windows.tf
+++ vm-win/template-windows.tf
@@ -58,7 +58,7 @@ resource "vsphere_tag" "region" {
 */
 
 resource "vsphere_virtual_machine" "vm" {
-  name             = "jsp-win-001"
+  name             = "jsp-win-1440"
   folder           = "Workloads"
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   datastore_id     = data.vsphere_datastore.datastore.id
